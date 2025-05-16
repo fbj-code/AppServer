f3b26cb28194153e2ef249b0902081f35475f90d
fbj <fbj>
2025-05-16 20:37:19 +0800

    初始提交

---
+++ src/main/java/example/fbjappserver/FbjAppServerApplication.java
@@ -0,0 +1,13 @@
+package example.fbjappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbjAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbjAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/fbjappserver/FbjAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package example.fbjappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbjAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
